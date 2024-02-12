848edaaa4081f9fcf1f37e0337de5f075c8a493e
“Anatoli <humanities5050@gmail.com>
2024-02-12 23:32:02 +0300

    feat: complete sortByAsc

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -337,8 +337,20 @@ function rotateMatrix(/* matrix */) {
  *  [2, 9, 5, 9]    => [2, 5, 9, 9]
  *  [-2, 9, 5, -3]  => [-3, -2, 5, 9]
  */
-function sortByAsc(/* arr */) {
-  throw new Error('Not implemented');
+function sortByAsc(arr) {
+  const result = arr;
+  for (let i = 0; i < result.length; i += 1) {
+    let indMin = i;
+    for (let j = i + 1; j < result.length; j += 1) {
+      if (result[j] < result[indMin]) {
+        indMin = j;
+      }
+    }
+    const temp = result[i];
+    result[i] = result[indMin];
+    result[indMin] = temp;
+  }
+  return result;
 }
 
 /**
